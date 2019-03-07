aff7d65278513617fc812150ee27007c1fb5df02
Mike Lythgoe <mike.lythgoe@bjss.com>
2019-03-07 09:21:13 +0000

    Improving layout and setting package private access on some classes and methods.

--- src/main/java/net/mikelythgoe/checkoutkata/CheckoutProcessor.java
+++ src/main/java/net/mikelythgoe/checkoutkata/CheckoutProcessor.java
@@ -25,16 +25,6 @@ public class CheckoutProcessor {
 
     public BigDecimal calculateCartTotalCost() {
 
-//        List<Person> people = Arrays.asList(new Person("Steve", "wine"), new Person("Steve", "cola"),
-//                new Person("Ben", "cola"), new Person("Ben", "cola"), new Person("Steve", "wine"),
-//                new Person("Steve", "wine"));
-//
-//        Map<Person, Long> map = people.stream()
-//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-//
-//        Map<String, Integer> map = items.stream()
-//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
         Repository repository = new Repository();
 
         BigDecimal totalCost = new BigDecimal("0.00");
